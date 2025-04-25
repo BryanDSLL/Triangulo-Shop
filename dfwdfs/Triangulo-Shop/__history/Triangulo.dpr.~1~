35cdbe10ed2d52b016f9503a0c76608715872d8d
BryanDSLL <bryandaimex@gmail.com>
2025-04-25 11:33:57 -0300

    'triangulo-shop'

---
+++ dfwdfs/Triangulo-Shop/__history/Triangulo.dpr.~1~
@@ -0,0 +1,14 @@
+program Triangulo;
+
+uses
+  Vcl.Forms,
+  frmTriangulo in 'frmTriangulo.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ dfwdfs/Triangulo/TipoTriangulo.pas
@@ -0,0 +1,17 @@
+unit TipoTriangulo;
+
+interface
+
+uses
+  Classes;
+
+type
+  TTipoTriangulo = interface
+  ['{A0491DB7-4025-421C-A29D-EACB22026B5B}']
+    function GetTipoTriangulo: String;
+    function GetImagemTriangulo: TMemoryStream;
+  end;
+
+implementation
+
+end.
